5d866a468b54b6fd82c326ee48ac7ef95a62a35c
_vishwaraj <vishwarajchundawat008@gmail.com>
2025-08-05 15:37:38 +0530

    docker test

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/generate-docs', generateDocsRoute);
+app.get("/", (req, res) => {
+    res.send("Welcome to MOAD")
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
